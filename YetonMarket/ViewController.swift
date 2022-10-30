54a7c908d826f4dad7f9cd0981447e2c8ad769c6
yeeton37 <lyeeun37@naver.com>
2022-10-31 00:40:03 +0900

    Initial Commit

---
+++ YetonMarket/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YetonMarket
+//
+//  Created by 이예은 on 2022/10/31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
